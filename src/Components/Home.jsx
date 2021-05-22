49149f08a5930b0b64492741b5cff17d6174c90b
Subham Das <subhamdas461@gmail.com>
2021-05-23 00:20:56 +0530

    home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,19 @@
 function Home(){
     return(
         <div id="home" className="home section">
-            {/* <h1>SUBHAM DAS (home)</h1> */}
+            <div className="home-content">
+                <div className="profile-img"></div>
+               
+                <p className="home-name">Hello! I'm Subham Das</p>
+                <p className="home-info">Student, Web Developer.</p>
+                <p className="resume">Resume</p>
+                <div className="social-icons">
+                    <i className="fab fa-facebook-f"></i>
+                    <i className="fab fa-instagram"></i>
+                    <i className="fab fa-twitter"></i>
+                    <i className="fab fa-github"></i>
+                </div>
+            </div>
         </div>
     )
 }
