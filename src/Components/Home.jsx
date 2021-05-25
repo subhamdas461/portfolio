78b7da0842423bcf2c5d30c254c191e9f3b64755
Subham Das <subhamdas461@gmail.com>
2021-05-25 20:41:29 +0530

    social

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,14 +4,14 @@ function Home(){
             <div className="home-content">
                 <div className="profile-img"></div>
                
-                <p className="home-name">Hi ! I'm Subham Das</p>
+                <p className="home-name">Hi, I'm Subham Das</p>
                 <p className="home-info">CS Student, Web Developer.</p>
                 <p className="resume">Resume</p>
                 <div className="social-icons">
-                    <i className="fab fa-facebook-f"></i>
-                    <i className="fab fa-linkedin"></i>
-                    <i className="fab fa-instagram"></i>
-                    <a href="https://github.com/subhamdas461"><i className="fab fa-github"></i></a>
+                    <a href="https://www.facebook.com/subham.das.148553/" target="_blank"><i className="fab fa-facebook-f"></i></a>
+                    <a href="https://linkedin.com/in/subhamdas461" target="_blank"><i className="fab fa-linkedin"></i></a>
+                    <a href="https://www.instagram.com/subba.JH" target="_blank"><i className="fab fa-instagram"></i></a>
+                    <a href="https://github.com/subhamdas461" target="_blank"><i className="fab fa-github"></i></a>
                 </div>
             </div>
         </div>
